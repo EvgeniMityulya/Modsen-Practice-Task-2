0ae886b41b8eb16dae7a15cb4a173f27c803fb58
iseedeadpeople <evgenni2004@gmail.com>
2024-06-18 11:11:24 +0300

    INIT

---
+++ Onboarding/Onboarding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Onboarding
+//
+//  Created by Евгений Митюля on 6/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
